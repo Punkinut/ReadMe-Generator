7620d7641c7b20847f98412539fc8004cdfef3af
Punkinut <heypunkinut@gmail.com>
2021-04-27 12:49:06 +1000

    Added basic run through

--- Develop/index.js
+++ Develop/index.js
@@ -1,2 +1,25 @@
 const enquirer = require('enquirer');
 const fs = require('fs');
+const { title } = require('process');
+
+let questions = [
+"What it the title of your project?", 
+"What is your GitHub username?",
+"Give me a brief description on the project you are working on.",
+"Give an explanation on how to install your application.",
+"Please provide some steps on the usage of this appliaction"];
+
+const [ qOne, qTwo, qThree, qFour, qFive ] = questions;
+
+enquirer
+    .prompt([
+        {
+            type: 'input',
+            message: qOne,
+            name: "project"
+        },
+    ])
+    .then((response) => {
+        const { project } =  response;
+        console.log(project)
+    });
