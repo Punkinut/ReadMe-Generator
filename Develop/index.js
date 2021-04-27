006bc5e38780cca0e758675910d6c278cc05828b
Punkinut <heypunkinut@gmail.com>
2021-04-27 12:59:17 +1000

    Added another question

--- Develop/index.js
+++ Develop/index.js
@@ -4,11 +4,12 @@ const fs = require('fs');
 let questions = [
 "What it the title of your project?", 
 "What is your GitHub username?",
+"What is your email?",
 "Give me a brief description on the project you are working on.",
 "Give an explanation on how to install your application.",
 "Please provide some steps on the usage of this appliaction"];
 
-const [ qOne, qTwo, qThree, qFour, qFive ] = questions;
+const [ qOne, qTwo, qThree, qFour, qFive, qSix ] = questions;
 
 enquirer
     .prompt([
