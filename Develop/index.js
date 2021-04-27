64a3feca3b86901defee152b4dbb213afa4a942f
Punkinut <heypunkinut@gmail.com>
2021-04-27 14:31:41 +1000

    Added all basic questions in array

--- Develop/index.js
+++ Develop/index.js
@@ -5,21 +5,30 @@ let questions = [
 "What it the title of your project?", 
 "What is your GitHub username?",
 "What is your email?",
-"Give me a brief description on the project you are working on.",
-"Give an explanation on how to install your application.",
-"Please provide some steps on the usage of this appliaction"];
+"Which liscence will you require for this repository?",
+"What was your motivation for this task?",
+"Why did you build this project?",
+"What problem does it solve?",
+"What did you learn?",
+"Press enter to explain your installation in three steps (press enter)",
+"Step One:",
+"Step Two:",
+"Step Three:",
+"Please provide some steps on how to use this appliaction",
+"Which code of conduct would you like to include in your contribution section? ",
+"Please add some final instructions on how to run tests on your application (if not applicable press enter)"];
 
-const [ qOne, qTwo, qThree, qFour, qFive, qSix ] = questions;
+const [ q1, q2, q3, q4, q5, q6, q7 ] = questions;
 
 enquirer
     .prompt([
         {
             type: 'input',
-            message: qOne,
+            message: q1,
             name: "project"
         },
     ])
     .then((response) => {
         const { project } =  response;
-        console.log(project)
+        console.log(project )
     });
